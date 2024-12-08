01a56bebf161962d0442ee0840ada31e1f4455e4
Pete Moore <pmoore@mozilla.com>
2024-12-08 12:06:12 +0100

    wip

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
-#!/bin/bash -eu
+#!/usr/bin/env bash
+
+set -eu
 
 cd "$(dirname "${0}")"
 rm -r dist
@@ -8,7 +10,9 @@ mv raspi-firmware-1.20241126/boot dist
 rm -r raspi-firmware{-1.20241126,_1.20241126.orig.tar.xz}
 aarch64-none-elf-as -g -o pcie-rpi400.o pcie-rpi400.s 
 aarch64-none-elf-ld -M -o pcie-rpi400.elf pcie-rpi400.o
-aarch64-none-elf-objcopy --set-start=0x80000 pcie-rpi400.elf -O binary dist/pcie-rpi400.img
-aarch64-none-elf-objdump -b binary -z --adjust-vma=0x80000 -maarch64 -D dist/pcie-rpi400.img
+aarch64-none-elf-objcopy --set-start=0x0 pcie-rpi400.elf -O binary dist/pcie-rpi400.img
+aarch64-none-elf-objdump -b binary -z --adjust-vma=0x0 -maarch64 -D dist/pcie-rpi400.img
 rm pcie-rpi400.{o,elf}
 cp config.txt dist
+
+echo 'All done!'
--- pcie-rpi400.s
+++ pcie-rpi400.s
@@ -7,8 +7,13 @@ _start:
   and     x0, x0, #0x3             // x0 = core number.
   cbnz    x0, sleep_core           // Put all cores except core 0 to sleep.
 
+  mov     x0, #0x00000000ff800000
+  str     wzr, [x0]
+  mov     w1, #0x0000000080000000
+  str     w1, [x0, #8]
+
   mrs     x0, s3_1_c11_c0_2        // l2ctlr_el1
-  mov     x1, #0x22
+  mov     x1, #0x0000000000000022
   orr     x0, x0, x1
   msr     s3_1_c11_c0_2, x0
 
@@ -19,26 +24,56 @@ _start:
 
   msr     cptr_el3, xzr
 
-  mov     x0, #0x531
+  mov     x0, #0x0000000000000531
   msr     scr_el3, x0
 
-  mov     x0, #0x70000
+  mov     x0, #0x0000000000070000  // TODO
   msr     vbar_el3, x0
 
-  mov     x0, #0x40
+  mov     x0, #0x0000000000000040
   msr     s3_1_c15_c2_1, x0        // cpuectlr_el1 
 
-  ldr     x0, =0x30c50830
+// setup gic
+  mrs     x0, mpidr_el1
+  ldr     x2, =0x00000000ff841000
+  tst     x0, #0x0000000000000003
+  b.eq    1f
+  mov     w0, #0x3
+  str     w0, [x2]
+1:
+  add     x1, x2, #0x0000000000001000
+  mov     w0, #0x000001e7
+  str     w0, [x1]
+  mov     w0, #0x000000ff
+  str     w0, [x1, #4]
+  add     x2, x2, #0x80
+  mov     x0, #0x20
+  mov     w1, #0xffffffff
+  2:
+    subs    x0, x0, #0x4
+    str     w1, [x2, x0]
+    b.ne    2b
+
+  ldr     x0, =0x0000000030c50830
   msr     sctlr_el2, x0
 
-  mov     x0, #0x3c9
+  mov     x0, #0x00000000000003c9
   msr     spsr_el3, x0
 
-  ldr     x0, =0x00000000000af000
+  adr     x0, 3f
+  msr     elr_el3, x0
+  eret
+
+3:
+
+  ldr     x0, =0x0000000000080090  // TODO
+  msr     sp_el1, x0
+
+  ldr     x0, =0x00000000000af000  // TODO
   msr     vbar_el2, x0
 
   mrs     x0, cnthctl_el2
-  orr     x0, x0, #0x3
+  orr     x0, x0, #0x0000000000000003
   msr     cnthctl_el2, x0
 
   msr     cntvoff_el2, xzr
@@ -49,49 +84,53 @@ _start:
   mrs     x1, mpidr_el1
   msr     vmpidr_el2, x1
 
-  mov     x0, #0x33ff
+  mov     x0, #0x00000000000033ff
   msr     cptr_el2, x0
 
   msr     hstr_el2, xzr
 
-  mov     x0, #0x300000
+  mov     x0, #0x0000000000300000
   msr     cpacr_el1, x0
 
-  mov     x0, #0x80000000
+  mov     x0, #0x0000000080000000
   msr     hcr_el2, x0
 
-  ldr     x0, =0x30d00800
+  ldr     x0, =0x0000000030d00800
   msr     sctlr_el1, x0
 
-  mov     x0, #0x3c4
+  mov     x0, #0x00000000000003c4
   msr     spsr_el2, x0
 
-  ldr     x0, =0x00000000000af000
+  adr     x0, 4f
+  msr     elr_el2, x0
+  eret
+
+4:
+
+  msr     daifclr, #0x1
+  msr     daifclr, #0x2
+
+  ldr     x0, =0x00000000000af000  // TODO
   msr     vbar_el1, x0
 
-  mov     x1, #0x4ff
+  mov     x1, #0x00000000000004ff
   msr     mair_el1, x1
 
   mov     x0, #0xffffffffffffffff  // TODO
   msr     ttbr0_el1, x0
 
   mrs     x0, tcr_el1
-  mov     x2, #0xffffffffffff0040
-  movk    x2, #0xffbf, lsl #16
-  movk    x2, #0xfff8, lsl #32
-  mov     x1, #0x751c
-  movk    x1, #0x80, lsl #16
-  and     x0, x0, x2
-  movk    x1, #0x1, lsl #32
-  orr     x0, x0, x1
+  ldr     x2, =0xfffffff9ffbf755c  // 0b1111111111111111111111111111100111111111101111110111010101011100
+  and     x0, x0, x2               // 0b-----------------------------00----------0------0---0-0-0-0---00
+  ldr     x1, =0x000000010080751c  // 0b0000000000000000000000000000000100000000100000000111010100011100
+  orr     x0, x0, x1               // 0b-----------------------------001--------10------011101010-011100
   msr     tcr_el1, x0
 
   mrs     x0, sctlr_el1
-  mov     x2, #0xfffffffffffffffd  //   (bit 1)
-  movk    x2, #0xfff7, lsl #16     //   (bit 19)
-  mov     x1, #0x1005              //   (bits 0, 2, 12)
-  and     x0, x0, x2               //   clear bits 1, 19
-  orr     x0, x0, x1               //   set bits 0, 2, 12
+  ldr     x2, =0xfffffffffff7fffd  // 0b1111111111111111111111111111111111111111111101111111111111111101
+  and     x0, x0, x2               // 0b--------------------------------------------0-----------------0-
+  mov     x1, #0x0000000000001005  // 0b0000000000000000000000000000000000000000000000000001000000000101
+  orr     x0, x0, x1               // 0b--------------------------------------------0------1---------101
   msr     sctlr_el1, x0
 
 sleep_core:
