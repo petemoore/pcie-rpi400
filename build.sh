794ff1f619100cb1793495db995a1b5740cfa848
Pete Moore <pmoore@mozilla.com>
2024-12-14 11:13:42 +0100

    wip

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 set -eu
+set -o pipefail
+export SHELLOPTS
+
+: ${AARCH64_TOOLCHAIN_PREFIX:=aarch64-none-elf-}
 
 cd "$(dirname "${0}")"
 rm -r dist
@@ -8,11 +12,12 @@ wget -q -O raspi-firmware_1.20241126.orig.tar.xz https://github.com/raspberrypi/
 tar xvfz raspi-firmware_1.20241126.orig.tar.xz
 mv raspi-firmware-1.20241126/boot dist
 rm -r raspi-firmware{-1.20241126,_1.20241126.orig.tar.xz}
-aarch64-none-elf-as -g -o pcie-rpi400.o pcie-rpi400.s 
-aarch64-none-elf-ld -M -o pcie-rpi400.elf pcie-rpi400.o
-aarch64-none-elf-objcopy --set-start=0x0 pcie-rpi400.elf -O binary dist/pcie-rpi400.img
-aarch64-none-elf-objdump -b binary -z --adjust-vma=0x0 -maarch64 -D dist/pcie-rpi400.img
-rm pcie-rpi400.{o,elf}
+
+"${AARCH64_TOOLCHAIN_PREFIX}as" -o pcie-rpi400.o pcie-rpi400.s
+"${AARCH64_TOOLCHAIN_PREFIX}ld" --no-warn-rwx-segments -N -Ttext=0x0 -o pcie-rpi400.elf pcie-rpi400.o
+"${AARCH64_TOOLCHAIN_PREFIX}objcopy" --set-start=0x0 pcie-rpi400.elf -O binary dist/pcie-rpi400.img
+"${AARCH64_TOOLCHAIN_PREFIX}objdump" -d pcie-rpi400.elf
+
 cp config.txt dist
 
 echo 'All done!'
--- pcie-rpi400.s
+++ pcie-rpi400.s
@@ -3,9 +3,6 @@
 .text
 
 _start:
-  mrs     x0, mpidr_el1            // x0 = Multiprocessor Affinity Register.
-  and     x0, x0, #0x3             // x0 = core number.
-  cbnz    x0, sleep_core           // Put all cores except core 0 to sleep.
 
   mov     x0, #0x00000000ff800000
   str     wzr, [x0]
@@ -63,6 +60,16 @@ _start:
   adr     x0, 3f
   msr     elr_el3, x0
   eret
+
+  mrs     x0, mpidr_el1            // x0 = Multiprocessor Affinity Register.
+  and     x0, x0, #0x3             // x0 = core number.
+  cbnz    x0, sleep_core           // Put all cores except core 0 to sleep.
+
+  adrp    x0, 0x2000000
+  mov     sp, x0
+  bl      uart_init
+  adr     x0, msg_hello_world
+  bl      uart_puts
 
 3:
   ldr     x0, =0x0000000000080090  // TODO
@@ -131,6 +138,12 @@ _start:
   orr     x0, x0, x1               // 0b--------------------------------------------0------1---------101
   msr     sctlr_el1, x0
 
+  b       sleep_core
+
+msg_hello_world:
+.asciz    "Hello World\n"
+
+.align 2
 sleep_core:
   wfe                              // Sleep until woken.
   b       sleep_core               // Go back to sleep.
@@ -158,6 +171,206 @@ HVCStub:
 
 ExceptionHandler:
   b       sleep_core
+
+# ------------------------------------------------------------------------------
+# See "BCM2837 ARM Peripherals" datasheet pages 90-104:
+#   https://cs140e.sergio.bz/docs/BCM2837-ARM-Peripherals.pdf
+# ------------------------------------------------------------------------------
+# GPFSEL1,        0x0004                       // GPIO Function Select 1
+# GPPUD,          0x0094                       // GPIO Pin Pull-up/down Enable
+# GPPUDCLK0,      0x0098                       // GPIO Pin Pull-up/down Enable Clock 0
+
+.text
+
+# ------------------------------------------------------------------------------
+# Initialise the Mini UART interface for logging over serial port.
+# Note, this is Broadcomm's own UART, not the ARM licenced UART interface.
+# ------------------------------------------------------------------------------
+.align 2
+uart_init:
+  adrp    x1, 0xfe215000
+  ldr     w2, [x1, #0x4]                          // w2 = [AUX_ENABLES] (Auxiliary enables)
+  orr     w2, w2, #1
+  str     w2, [x1, #0x4]                          //   [AUX_ENABLES] |= 0x00000001 => Enable Mini UART.
+  str     wzr, [x1, #0x44]                        //   [AUX_MU_IER] = 0x00000000 => Disable Mini UART interrupts.
+  str     wzr, [x1, #0x60]                        //   [AUX_MU_CNTL] = 0x00000000 => Disable Mini UART Tx/Rx
+  mov     w2, #0x6                                // w2 = 6
+  str     w2, [x1, #0x48]                         //   [AUX_MU_IIR] = 0x00000006 => Mini UART clear Tx, Rx FIFOs
+  mov     w3, #0x3                                // w3 = 3
+  str     w3, [x1, #0x4c]                         //   [AUX_MU_LCR] = 0x00000003 => Mini UART in 8-bit mode.
+  str     wzr, [x1, #0x50]                        //   [AUX_MU_MCR] = 0x00000000 => Set UART1_RTS line high.
+  mov     w2, 0x0000021d
+  str     w2, [x1, #0x68]                         //   [AUX_MU_BAUD] = 0x0000010e (rpi3) or 0x0000021d (rpi4)
+                                                  //         => baudrate = system_clock_freq/(8*([AUX_MU_BAUD]+1))
+                                                  //                       (as close to 115200 as possible)
+  adrp    x4, 0xfe200000
+  ldr     w2, [x4, #0x4]                          // w2 = [GPFSEL1]
+  and     w2, w2, #0xfffc0fff                     // Unset bits 12, 13, 14 (FSEL14 => GPIO Pin 14 is an input).
+                                                  // Unset bits 15, 16, 17 (FSEL15 => GPIO Pin 15 is an input).
+  orr     w2, w2, #0x00002000                     // Set bit 13 (FSEL14 => GPIO Pin 14 takes alternative function 5).
+  orr     w2, w2, #0x00010000                     // Set bit 16 (FSEL15 => GPIO Pin 15 takes alternative function 5).
+  str     w2, [x4, #0x4]                          //   [GPFSEL1] = updated value => Enable UART 1.
+  str     wzr, [x4, #0x94]                        //   [GPPUD] = 0x00000000 => GPIO Pull up/down = OFF
+  mov     x5, #0x96                               // Wait 150 instruction cycles (as stipulated by datasheet).
+1:
+  subs    x5, x5, #0x1                            // x0 -= 1
+  b.ne    1b                                      // Repeat until x0 == 0.
+  mov     w2, #0xc000                             // w2 = 2^14 + 2^15
+  str     w2, [x4, #0x98]                         //   [GPPUDCLK0] = 0x0000c000 => Control signal to lines 14, 15.
+  mov     x0, #0x96                               // Wait 150 instruction cycles (as stipulated by datasheet).
+2:
+  subs    x0, x0, #0x1                            // x0 -= 1
+  b.ne    2b                                      // Repeat until x0 == 0.
+  str     wzr, [x4, #0x98]                        //   [GPPUDCLK0] = 0x00000000 => Remove control signal to lines 14, 15.
+  str     w3, [x1, #0x60]                         //   [AUX_MU_CNTL] = 0x00000003 => Enable Mini UART Tx/Rx
+  ret                                             // Return.
+
+
+# ------------------------------------------------------------------------------
+# Send a byte over Mini UART
+# ------------------------------------------------------------------------------
+# On entry:
+#   x0: char to send
+# On exit:
+#   x1: 0xfe215000
+#   x2: Last read of [AUX_MU_LSR] when waiting for bit 5 to be set
+uart_send:
+  adrp    x1, 0xfe215000
+1:
+  ldr     w2, [x1, #0x54]                         // w2 = [AUX_MU_LSR]
+  tbz     x2, #5, 1b                              // Repeat last statement until bit 5 is set.
+  strb    w0, [x1, #0x40]                         //   [AUX_MU_IO] = w0
+  ret
+
+# ------------------------------------------------------------------------------
+# Send '\r\n' over Mini UART
+# ------------------------------------------------------------------------------
+#
+# On entry:
+#   <nothing>
+# On exit:
+#   x0: 0x0a
+#   x1: 0xfe215000
+#   x2: Last read of [AUX_MU_LSR] when waiting for bit 5 to be set
+uart_newline:
+  stp     x29, x30, [sp, #-16]!                   // Push frame pointer, procedure link register on stack.
+  mov     x29, sp                                 // Update frame pointer to new stack location.
+  mov     x0, #13
+  bl      uart_send
+  mov     x0, #10
+  bl      uart_send
+  ldp     x29, x30, [sp], #0x10                   // Pop frame pointer, procedure link register off stack.
+  ret
+
+
+# ------------------------------------------------------------------------------
+# Send a null terminated string over Mini UART.
+# ------------------------------------------------------------------------------
+#
+# On entry:
+#   x0 = address of null terminated string
+# On exit:
+#   x0 = address of null terminator
+#   x1 = 0xfe215000
+#   x2 = 0
+#   x3 = [AUX_MU_LSR]
+uart_puts:
+  adrp     x1, 0xfe215000
+1:
+  ldrb    w2, [x0], #1
+  cbz     w2, 5f
+  cmp     w2, #127
+  b.ne    4f
+  mov     w2, '('
+2:
+  ldr     w3, [x1, #0x54]                         // w3 = [AUX_MU_LSR]
+  tbz     x3, #5, 2b                              // Repeat last statement until bit 5 is set.
+  strb    w2, [x1, #0x40]                         //   [AUX_MU_IO] = w2
+  mov     w2, 'c'
+3:
+  ldr     w3, [x1, #0x54]                         // w3 = [AUX_MU_LSR]
+  tbz     x3, #5, 3b                              // Repeat last statement until bit 5 is set.
+  strb    w2, [x1, #0x40]                         //   [AUX_MU_IO] = w2
+  mov     w2, ')'
+4:
+  ldr     w3, [x1, #0x54]                         // w3 = [AUX_MU_LSR]
+  tbz     x3, #5, 4b                              // Repeat last statement until bit 5 is set.
+  strb    w2, [x1, #0x40]                         //   [AUX_MU_IO] = w2
+  b       1b
+5:
+  ret
+
+
+# ------------------------------------------------------------------------------
+# Write the full value of x0 as hex string (0x0123456789abcdef) to Mini UART.
+# Sends 18 bytes ('0', 'x', <16 byte hex string>). No trailing newline.
+# ------------------------------------------------------------------------------
+#
+# On entry:
+#   x0 = value to write as a hex string to Mini UART.
+#   x2 = number of bits to print (multiple of 4)
+uart_x0:
+  stp     x29, x30, [sp, #-16]!                   // Push frame pointer, procedure link register on stack.
+  mov     x29, sp                                 // Update frame pointer to new stack location.
+  mov     x2, #64
+  bl      uart_x0_s
+  ldp     x29, x30, [sp], #16                     // Pop frame pointer, procedure link register off stack.
+  ret
+
+
+# ------------------------------------------------------------------------------
+# Write the lower nibbles of x0 as a hex string to Mini UART, with custom size
+# (number of nibbles).
+# ------------------------------------------------------------------------------
+#
+# Includes leading '0x' and no trailing newline.
+#
+# On entry:
+#   x0 = value to write as a hex string to Mini UART.
+#   x2 = number of lower bits of x0 to print (multiple of 4)
+uart_x0_s:
+  stp     x29, x30, [sp, #-16]!                   // Push frame pointer, procedure link register on stack.
+  mov     x29, sp                                 // Update frame pointer to new stack location.
+  stp     x19, x20, [sp, #-16]!                   // Backup x19, x20
+  mov     x19, x0                                 // Backup x0 in x19
+  sub     sp, sp, #0x20                           // Allocate space on stack for hex string
+  mov     w3, #0x7830
+  mov     x1, sp
+  strh    w3, [x1], #2                            // "0x"
+  bl      hex_x0
+  strb    wzr, [x1], #1
+  mov     x0, sp
+  bl      uart_puts
+  add     sp, sp, #0x20
+  mov     x0, x19                                 // Restore x0
+  ldp     x19, x20, [sp], #16                     // Restore x19, x20
+  ldp     x29, x30, [sp], #16                     // Pop frame pointer, procedure link register off stack.
+  ret
+
+# On entry:
+#   x0 = hex value to convert to text
+#   x1 = address to write text to (no trailing 0)
+#   x2 = number of bits to convert (multiple of 4)
+# On exit:
+#   x0 = <unchanged>
+#   x1 = address of next unused char (x1 += x2/4)
+#   x2 = 0
+#   x3 = last char in text
+hex_x0:
+  ror     x0, x0, x2
+1:
+  ror     x0, x0, #60
+  and     w3, w0, #0x0f
+  add     w3, w3, 0x30
+  cmp     w3, 0x3a
+  b.lo    2f
+  add     w3, w3, 0x27
+2:
+  strb    w3, [x1], #1
+  subs    w2, w2, #4
+  b.ne    1b
+  ret
+
 
 .align 12
 VectorTableEL3:
